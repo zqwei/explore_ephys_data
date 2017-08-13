95a4f3c5b0ad0b0d223441344bd78cf4dee39b2b
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-08-13 15:14:45 -0400

    add plot of raster

---
+++ plot_raster.m
@@ -0,0 +1,41 @@
+% plot rasters 
+% 
+% This code will go through raster plot for a selected single neuron at
+% for each behavioral conditions.
+%
+% blue: lick right
+% red: lick left
+% 
+%
+% Ziqiang Wei
+% weiz@janelia.hhmi.org
+
+% cell number (ranged from 1 to length of datasets)
+cellId = 10;
+
+unit_yes_trial_spk_time = ephysDataset(cellId).unit_yes_trial_spk_time;
+
+figure;
+
+title(['Raster plot for cell #' num2str(cellId)]);
+
+hold on;
+
+numTrial = 0;
+
+for ntrial = 1:length(unit_yes_trial_spk_time)
+    spkTime = unit_yes_trial_spk_time{ntrial};
+    numTrial = numTrial + 1;
+    plot(spkTime, numTrial * ones(length(spkTime), 1), '.b');
+end
+
+unit_no_trial_spk_time = ephysDataset(cellId).unit_no_trial_spk_time;
+
+for ntrial = 1:length(unit_yes_trial_spk_time)
+    spkTime = unit_no_trial_spk_time{ntrial};
+    numTrial = numTrial + 1;
+    plot(spkTime, numTrial * ones(length(spkTime), 1), '.r');
+end
+
+gridxy([-2.6 -1.3 0],'Color','k','Linestyle','--') ;
+
