a227906dfcfcd1da74b66f5f1556c8cea2167d22
Ziqiang Wei <weiz@janelia.hhmi.org>
2021-02-27 12:55:28 -0500

    Update func_plot_PSTH.m
    change field name according to the new data (see `change_field_names.m` for details)

--- func_plot_PSTH.m
+++ func_plot_PSTH.m
@@ -16,8 +16,8 @@ function [ PSTH ] = func_plot_PSTH (cellId,timeTag,ephysDataset)
 % PSTH: PSTH of lick R trials (1st row) and lick L trials  (2nd row)
 
     %% Calculate the mean spike rate
-    meanR = mean(ephysDataset(cellId).unit_yes_trial,1);
-    meanL = mean(ephysDataset(cellId).unit_no_trial,1);
+    meanR = mean(ephysDataset(cellId).sr_right,1);
+    meanL = mean(ephysDataset(cellId).sr_left,1);
     PSTH  = [meanR;meanL];
 
     %% plot the PSTH
